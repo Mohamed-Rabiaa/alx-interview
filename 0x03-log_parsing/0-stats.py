045183795ba978fbaa2738b1f94c2026c7a8fc73
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-08-15 14:16:27 +0300

    Fixing docstring

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python3
 """
+This script reads stdin line by line and computes metrics:
+Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
+ <status code> <file size>
+(if the format is not this one, the line will be skipped)
+
+After every 10 lines and/or a keyboard interruption (CTRL + C),
+prints these statistics from the beginning:
+        Total file size: File size: <total size>
+        where <total size> is the sum of all previous <file size>.
+        Number of lines by status code:
+            possible status code: 200, 301, 400, 401, 403, 404, 405 and 500.
+            if a status code doesn’t appear or is not an integer,
+            we won’t print anything for this status code.
+            format: <status code>: <number>
+            status codes will be printed in ascending order
 """
 
 import sys
